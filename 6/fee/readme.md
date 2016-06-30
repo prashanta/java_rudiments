d461a047f239077aef147ffd371d34ec3951d75d
iprashanta <pmshrestha@gmail.com>
2016-06-30 16:47:29 +0700

    Added part 5 and 6

---
+++ 6/fee/readme.md
@@ -0,0 +1,14 @@
+### To Start
+
+mvn archetype:generate -DgroupId=com.foo.bar -DartifactId=fee 
+-DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
+
+### To Compile
+
+mvn package
+
+### To Run
+
+java -cp target/fee-1.0-SNAPSHOT.jar com.foo.bar.App
+
+
---
+++ 6/fee/src/main/java/com/foo/bar/App.java
@@ -0,0 +1,18 @@
+package com.foo.bar;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Logger logger = LoggerFactory.getLogger(App.class);
+	logger.info("Hello World!");
+    }
+}
